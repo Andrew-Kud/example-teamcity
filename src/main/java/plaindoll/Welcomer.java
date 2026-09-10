830788743ef75c7ad398f4159ca1670ecfff3147
Andrey Kudryashov <masadov.dr@gmail.com>
2026-09-10 12:02:52 +0300

    add hunter reply-test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,18 +1,39 @@
 package plaindoll;
 
-public class Welcomer{
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Welcomer {
+
+    public String sayWelcome() {
+        return "Welcome home, good hunter. What is it your desire?";
+    }
+
+    public String sayFarewell() {
+        return "Farewell, good hunter. May you find your worth in waking world.";
+    }
+
+    public String sayNeedGold() {
+        return "Not enough gold";
+    }
+
+    public String saySome() {
+        return "something in the way";
+    }
+
+    public String sayFeniks() {
+        return "This hunter is name is Petrovich!";
+    }
+
+    public String getHunterReply() {
+        List<String> replies = List.of(
+            "Hello, hunter!",
+            "Good luck, hunter!",
+            "Welcome back, hunter!"
+        );
+
+        return replies.get(
+            ThreadLocalRandom.current().nextInt(replies.size())
+        );
+    }
 }
